6a354374b3f7effb6e2e3ef40e1078e8e65c8301
ilaedna16 <115794672+ilaedna16@users.noreply.github.com>
2022-11-16 17:22:28 -0300

    Add files via upload

---
+++ main (5).c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+   int idade;
+    int d; 
+ 
+   
+   
+   printf ("digite a sua idade\n"); 
+   scanf ("%d", &idade); 
+   
+   printf ("informe o total de dias de vida\n"); 
+   scanf ("%d",&d); 
+   
+   
+   
+ idade=idade;
+ 
+ 
+ printf ("o usuario possui:\n %d", idade); 
+    
+  printf (" dias de vida eh:\n %d", d);   
+    return 0;
+}
